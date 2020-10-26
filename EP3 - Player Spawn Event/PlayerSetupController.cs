16e8a08121a4873ffccf3d03be7b5b71502f4373
Boltlorant <boltlorant@gmail.com>
2020-10-26 18:35:43 +0100

    EP3

---
+++ EP3 - Player Spawn Event/PlayerSetupController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Bolt;
+
+public class PlayerSetupController : GlobalEventListener
+{
+    [SerializeField]
+    private GameObject _setupPanel;
+
+    public override void SceneLoadLocalDone(string scene, IProtocolToken token)
+    {
+        if (!BoltNetwork.IsServer)
+        {
+            _setupPanel.SetActive(true);
+        }
+    }
+
+    public override void OnEvent(SpawnPlayerEvent evnt)
+    {
+        BoltEntity entity = BoltNetwork.Instantiate(BoltPrefabs.Player, new Vector3(0, 1, 0), Quaternion.identity);
+        entity.AssignControl(evnt.RaisedBy);
+    }
+
+    public void SpawnPlayer()
+    {
+        SpawnPlayerEvent evnt = SpawnPlayerEvent.Create(GlobalTargets.OnlyServer);
+        evnt.Send();
+    }
+}
