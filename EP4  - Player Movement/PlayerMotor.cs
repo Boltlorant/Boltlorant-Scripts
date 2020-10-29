b2aa6b4f7bb4f6432141ddc2a37d6acd0f4ca687
Boltlorant <boltlorant@gmail.com>
2020-10-29 17:58:53 +0100

    EP4

---
+++ EP4  - Player Movement/PlayerMotor.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    [SerializeField]
+    private Camera _cam = null;
+    private Rigidbody _rigidbody = null;
+
+    private float _speed = 7f;
+
+    private Vector3 _lastServerPos = Vector3.zero;
+    private bool _firstState = true;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Init(bool isMine)
+    {
+        if (isMine)
+            _cam.gameObject.SetActive(true);
+    }
+
+    public State ExecuteCommand(bool forward, bool backward, bool left, bool right, float yaw, float pitch)
+    {
+        Vector3 movingDir = Vector3.zero;
+        if (forward ^ backward)
+        {
+            movingDir += forward ? transform.forward : -transform.forward;
+        }
+        if (left ^ right)
+        {
+            movingDir += right ? transform.right : -transform.right;
+        }
+
+        movingDir.Normalize();
+        movingDir *= _speed;
+        _rigidbody.velocity = movingDir;
+
+        _cam.transform.localEulerAngles = new Vector3(pitch, 0f, 0f);
+        transform.rotation = Quaternion.Euler(0, yaw, 0);
+
+        State stateMotor = new State();
+        stateMotor.position = transform.position;
+        stateMotor.rotation = yaw;
+
+        return stateMotor;
+    }
+
+    public void SetState(Vector3 position, float rotation)
+    {
+        if (Mathf.Abs(rotation - transform.rotation.y) > 5f)
+            transform.rotation = Quaternion.Euler(0, rotation, 0);
+
+        if (_firstState)
+        {
+            if (position != Vector3.zero)
+            {
+                transform.position = position;
+                _firstState = false;
+                _lastServerPos = Vector3.zero;
+            }
+        }
+        else
+        {
+            if (position != Vector3.zero)
+            {
+                _lastServerPos = position;
+            }
+
+            transform.position += (_lastServerPos - transform.position) * 0.5f;
+        }
+    }
+
+    public struct State
+    {
+        public Vector3 position;
+        public float rotation;
+    }
+}
---
+++ EP4  - Player Movement/PlayerSetupController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Bolt;
+
+public class PlayerSetupController : GlobalEventListener
+{
+    [SerializeField]
+    private Camera _sceneCamera;
+
+    [SerializeField]
+    private GameObject _setupPanel;
+
+    public Camera SceneCamera { get => _sceneCamera; }
+
+    public override void SceneLoadLocalDone(string scene, IProtocolToken token)
+    {
+        if (!BoltNetwork.IsServer)
+        {
+            _setupPanel.SetActive(true);
+        }
+    }
+
+    public override void OnEvent(SpawnPlayerEvent evnt)
+    {
+        BoltEntity entity = BoltNetwork.Instantiate(BoltPrefabs.Player, new Vector3(0, 1, 0), Quaternion.identity);
+        entity.AssignControl(evnt.RaisedBy);
+    }
+
+    public void SpawnPlayer()
+    {
+        SpawnPlayerEvent evnt = SpawnPlayerEvent.Create(GlobalTargets.OnlyServer);
+        evnt.Send();
+    }
+}
