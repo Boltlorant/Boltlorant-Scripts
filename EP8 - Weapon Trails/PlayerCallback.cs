e73726e7ff5c01089d8f67f017e9ade9381522f9
Boltlorant <boltlorant@gmail.com>
2020-11-12 18:45:21 +0100

    EP8

---
+++ EP8 - Weapon Trails/PlayerCallback.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using Bolt;
+
+public class PlayerCallback : EntityEventListener<IPlayerState>
+{
+    private PlayerMotor _playerMotor;
+    private PlayerWeapons _playerWeapons;
+
+    private void Awake()
+    {
+        _playerMotor = GetComponent<PlayerMotor>();
+        _playerWeapons = GetComponent<PlayerWeapons>();
+    }
+
+    public override void Attached()
+    {
+        state.AddCallback("LifePoints", UpdatePlayerLife);
+        state.AddCallback("Pitch", _playerMotor.SetPitch);
+
+        if (entity.IsOwner)
+        {
+            state.LifePoints = _playerMotor.TotalLife;
+        }
+    }
+
+    public void FireEffect(float precision, int seed)
+    {
+        FireEffectEvent evnt = FireEffectEvent.Create(entity, EntityTargets.EveryoneExceptOwnerAndController);
+        evnt.Precision = precision;
+        evnt.Seed = seed;
+        evnt.Send();
+    }
+
+    public override void OnEvent(FireEffectEvent evnt)
+    {
+        _playerWeapons.FireEffect(evnt.Seed, evnt.Precision);
+    }
+
+    public void UpdatePlayerLife()
+    {
+        if(entity.HasControl)
+            GUI_Controller.Current.UpdateLife(state.LifePoints, _playerMotor.TotalLife);
+    }
+}
---
+++ EP8 - Weapon Trails/WeaponStats.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Weapon", menuName = "Equipment/Weapon")]
+public class WeaponStats : ScriptableObject
+{
+    [Space(40)]
+    public string weaponName;
+    public float reloadTime = 1f;
+    public int magazin = 30;
+    public int totalMagazin = 120;
+    public int ammoPerShot = 1;
+    public int multiShot = 1;
+    [Space(10)]
+    public int rpm = 80;
+    public int dmg = 25;
+    public int maxRange = 30;
+    public float precision = 0.3f;
+    [Space(10)]
+    public GameObject trail;
+    public GameObject decal;
+    public GameObject impact;
+}
