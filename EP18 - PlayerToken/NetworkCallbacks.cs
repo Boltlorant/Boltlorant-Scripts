732538951555ac57138e5a0d8a837079b431d31c
Boltlorant <boltlorant@gmail.com>
2021-01-11 17:46:59 +0100

    EP18

---
+++ EP18 - PlayerToken/NetworkCallbacks.cs
@@ -0,0 +1,10 @@
+﻿[BoltGlobalBehaviour]
+public class NetworkCallbacks : Bolt.GlobalEventListener
+{
+    public override void BoltStartBegin()
+    {
+        BoltNetwork.RegisterTokenClass<Bolt.Photon.PhotonRoomProperties>();
+        BoltNetwork.RegisterTokenClass<WeaponDropToken>();
+        BoltNetwork.RegisterTokenClass<PlayerToken>();
+    }
+}
---
+++ EP18 - PlayerToken/PlayerToken.cs
@@ -0,0 +1,47 @@
+﻿public class PlayerToken : Bolt.IProtocolToken
+{
+    public string name;
+    public Team team;
+    public CharacterClass characterClass;
+    public PlayerSquadID playerSquadID;
+
+
+    public void Write(UdpKit.UdpPacket packet)
+    {
+        packet.WriteString(name);
+        packet.WriteShort((short)team);
+        packet.WriteShort((short)characterClass);
+        packet.WriteShort((short)playerSquadID);
+    }
+
+    public void Read(UdpKit.UdpPacket packet)
+    {
+        name = packet.ReadString();
+        team = (Team)packet.ReadShort();
+        characterClass = (CharacterClass)packet.ReadShort();
+        playerSquadID = (PlayerSquadID)packet.ReadShort();
+    }
+}
+
+public enum PlayerSquadID
+{
+    Orange,
+    Blue,
+    Green,
+    Purple,
+    Pink
+}
+
+public enum CharacterClass
+{
+    Soldier,
+    Medic,
+    Heavy
+}
+
+public enum Team
+{
+    TT,
+    AT,
+    None
+}
