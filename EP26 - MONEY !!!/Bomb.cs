391bb2cb95874dec7a4c7b6a42387742dc1ba238
Boltlorant <boltlorant@gmail.com>
2021-02-25 15:29:19 +0100

    EP26

---
+++ EP26 - MONEY !!!/Bomb.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : Weapon
+{
+    private GameController _gameController = null;
+
+    private float _plantingTime = 7.5f;
+    private bool _pressed = false;
+    private float _plantedTime = 0;
+
+    public override void Init(PlayerWeapons pw, int index)
+    {
+        base.Init(pw, 3);
+        if (_gameController == null)
+            _gameController = FindObjectOfType<GameController>();
+    }
+
+    private void OnEnable()
+    {
+        if (_playerMotor)
+            if (_playerMotor.entity.HasControl)
+                GUI_Controller.Current.HideAmmo();
+    }
+
+    private void OnDisable()
+    {
+        _pressed = false;
+        _plantedTime = 0;
+        _playerMotor.IsPlanting = false;
+        GUI_Controller.Current.PlantingProgressShow(false);
+    }
+
+    protected override void _Fire(int seed)
+    {
+
+    }
+
+    public override void ExecuteCommand(bool fire, bool aiming, bool reload, int seed)
+    {
+        if (_gameController.IsInSite)
+        {
+            if (fire)
+            {
+                if (_pressed == false)
+                {
+                    _playerMotor.IsPlanting = true;
+                    _pressed = true;
+                    _plantedTime = BoltNetwork.ServerTime + _plantingTime;
+                    GUI_Controller.Current.PlantingProgressShow(true);
+                }
+
+                if (_plantedTime < BoltNetwork.ServerTime && _plantedTime != 0)
+                {
+                    if (_playerWeapons.entity.IsOwner)
+                    {
+                        BoltNetwork.Instantiate(BoltPrefabs.BombGoal, transform.position, Quaternion.identity);
+                        _gameController.Planted();
+
+                        if (_playerMotor.state.Money + 600 > 8000)
+                            _playerMotor.state.Money = 8000;
+                        else
+                            _playerMotor.state.Money += 600;
+
+                        _playerWeapons.RemoveBomb();
+                    }
+                    _playerMotor.IsPlanting = false;
+                    GUI_Controller.Current.PlantingProgressShow(false);
+                    _plantedTime = 0;
+                }
+
+                if (_playerWeapons.entity.HasControl)
+                {
+                    GUI_Controller.Current.PlantingProgress(1 + ((BoltNetwork.ServerTime - _plantedTime) / _plantingTime));
+                }
+            }
+            else
+            {
+                if (_pressed)
+                {
+                    _playerMotor.IsPlanting = false;
+                    _pressed = false;
+                    _plantedTime = 0;
+                    GUI_Controller.Current.PlantingProgressShow(false);
+                }
+            }
+        }
+        else
+        {
+            _plantedTime = 0;
+            GUI_Controller.Current.PlantingProgressShow(false);
+        }
+    }
+}
---
+++ EP26 - MONEY !!!/PlayerCallback.cs
@@ -0,0 +1,199 @@
+﻿using UnityEngine;
+using Bolt;
+using System.Collections;
+
+public class PlayerCallback : EntityEventListener<IPlayerState>
+{
+    private PlayerMotor _playerMotor;
+    private PlayerWeapons _playerWeapons;
+    private PlayerController _playerController;
+    private PlayerRenderer _playerRenderer;
+
+    private void Awake()
+    {
+        _playerMotor = GetComponent<PlayerMotor>();
+        _playerWeapons = GetComponent<PlayerWeapons>();
+        _playerController = GetComponent<PlayerController>();
+        _playerRenderer = GetComponent<PlayerRenderer>();
+    }
+
+    public override void Attached()
+    {
+        state.AddCallback("LifePoints", UpdatePlayerLife);
+        state.AddCallback("Pitch", _playerMotor.SetPitch);
+        state.AddCallback("Energy", UpdateEnergy);
+        state.AddCallback("IsDead", UpdateDeathState);
+        state.AddCallback("WeaponIndex", UpdateWeaponIndex);
+        state.AddCallback("Money", UpdateMoney);
+        state.AddCallback("Weapons[].ID", UpdateWeaponList);
+        state.AddCallback("Weapons[].CurrentAmmo", UpdateWeaponAmmo);
+        state.AddCallback("Weapons[].TotalAmmo", UpdateWeaponAmmo);
+
+
+        if (entity.IsOwner)
+        {
+            state.IsDead = false;
+            state.LifePoints = _playerMotor.TotalLife;
+            state.Energy = 6;
+            state.Money = 500;
+            GameController.Current.UpdateGameState();
+            GameController.Current.state.AlivePlayers++;
+        }
+    }
+
+    public void UpdateWeaponList(IState state, string propertyPath, ArrayIndices arrayIndices)
+    {
+        int index = arrayIndices[0];
+        IPlayerState s = (IPlayerState)state;
+        if (s.Weapons[index].ID == -1)
+            _playerWeapons.RemoveWeapon(index);
+        else
+            _playerWeapons.AddWeapon((WeaponID)s.Weapons[index].ID);
+    }
+
+    public void UpdateWeaponAmmo(IState state, string propertyPath, ArrayIndices arrayIndices)
+    {
+        int index = arrayIndices[0];
+        IPlayerState s = (IPlayerState)state;
+        _playerWeapons.InitAmmo(index, s.Weapons[index].CurrentAmmo, s.Weapons[index].TotalAmmo);
+    }
+
+    public void UpdateWeaponIndex()
+    {
+        _playerController.Wheel = state.WeaponIndex;
+        _playerWeapons.SetWeapon(state.WeaponIndex);
+    }
+
+    public void FireEffect(float precision, int seed)
+    {
+        FireEffectEvent evnt = FireEffectEvent.Create(entity, EntityTargets.EveryoneExceptOwnerAndController);
+        evnt.Precision = precision;
+        evnt.Seed = seed;
+        evnt.Send();
+    }
+
+    public override void OnEvent(FireEffectEvent evnt)
+    {
+        _playerWeapons.FireEffect(evnt.Seed, evnt.Precision);
+    }
+
+    public void UpdatePlayerLife()
+    {
+        if(entity.HasControl)
+            GUI_Controller.Current.UpdateLife(state.LifePoints, _playerMotor.TotalLife);
+    }
+
+    public void UpdateEnergy()
+    {
+        if (entity.HasControl)
+        {
+            GUI_Controller.Current.UpdateAbilityView(state.Energy);
+        }
+    }
+
+    public void RaiseFlashEvent()
+    {
+        FlashEvent evnt = FlashEvent.Create(entity, EntityTargets.OnlyController);
+        evnt.Send();
+    }
+
+    public override void OnEvent(FlashEvent evnt)
+    {
+        GUI_Controller.Current.Flash();
+    }
+
+    private void UpdateDeathState()
+    {
+        if (entity.IsOwner)
+        {
+            if (state.IsDead)
+                GameController.Current.state.AlivePlayers--;
+            else
+                GameController.Current.state.AlivePlayers++;
+        }
+
+        if (entity.HasControl)
+            GUI_Controller.Current.Show(!state.IsDead);
+
+        _playerMotor.OnDeath(state.IsDead);
+        _playerRenderer.OnDeath(state.IsDead);
+        _playerWeapons.OnDeath(state.IsDead);
+    }
+
+    public void RoundReset(Team winner)
+    {
+        if (entity.IsOwner)
+        {
+            if (GameController.Current.CurrentPhase != GamePhase.Starting)
+            {
+                if (state.IsDead == true)
+                {
+                    state.IsDead = false;
+                    if (GameController.Current.CurrentPhase == GamePhase.WaitForPlayers)
+                    {
+                        state.Energy = 1;
+
+                        state.LifePoints = _playerMotor.TotalLife;
+                        state.SetTeleport(state.Transform);
+                        PlayerToken token = (PlayerToken)entity.AttachToken;
+                        transform.position = FindObjectOfType<PlayerSetupController>().GetSpawnPoint(token.team);
+                    }
+                }
+                else
+                {
+                    _playerWeapons.OnDeath(state.IsDead);
+                }
+
+                if (GameController.Current.CurrentPhase == GamePhase.StartRound)
+                {
+                    if (state.Energy < 4)
+                        state.Energy += 1;
+
+                    if (state.Money + 800 > 8000)
+                        state.Money = 8000;
+                    else
+                        state.Money += 800;
+
+                    PlayerToken token = (PlayerToken)entity.AttachToken;
+
+                    if (token.team == winner)
+                    {
+                        if (state.Money + 500 > 8000)
+                            state.Money = 8000;
+                        else
+                            state.Money += 500;
+                    }
+
+                    state.LifePoints = _playerMotor.TotalLife;
+                    state.SetTeleport(state.Transform);
+                    transform.position = FindObjectOfType<PlayerSetupController>().GetSpawnPoint(token.team);
+                }
+            }
+            else
+            {
+                state.Money = 0;
+                state.Energy = 0;
+            }
+        }
+    }
+
+    public void RaiseStartDifuseEvent(float time)
+    {
+        StartDifuseEvent evnt = StartDifuseEvent.Create(entity, EntityTargets.OnlyController);
+        evnt.Time = time;
+        evnt.Send();
+    }
+
+    private void UpdateMoney()
+    {
+        if (entity.HasControl)
+        {
+            GUI_Controller.Current.UpdateMoney(state.Money);
+        }
+    }
+
+    public override void OnEvent(StartDifuseEvent evnt)
+    {
+        _playerMotor.Difuse(evnt.Time);
+    }
+}
