e6ec16a159955e43765fd439b8a998844b69df36
Boltlorant <boltlorant@gmail.com>
2021-03-15 21:41:02 +0100

    EP28

---
+++ EP28 - Shop/UI_Shop.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UI_Shop : MonoBehaviour
+{
+    [SerializeField]
+    private ShopItem[] shopItems;
+    [SerializeField]
+    private Text _energyCount;
+    [SerializeField]
+    private UI_ShopItem _energyButton;
+    private int[] _energyCost = { 300, 600, 900, 1200, 2000, 2000, 2000 };
+    private string[] _energyView = { "", "*", "**", "***", "****", "*****", "******" };
+    private int _currentEnergy = 0;
+
+    void Start()
+    {
+        for (int i = 0; i < shopItems.Length; i++)
+        {
+            shopItems[i].button.Init(shopItems[i].cost);
+        }
+    }
+
+    public void BuyWeapon(int index)
+    {
+        GameController.Current.localPlayer.GetComponent<PlayerCallback>().RaiseBuyWeaponEvent(index);
+    }
+
+    public void BuyEnergy()
+    {
+        GameController.Current.localPlayer.GetComponent<PlayerCallback>().RaiseBuyEnergyEvent();
+    }
+
+    public int ItemCost(int index)
+    {
+        return shopItems[index].cost;
+    }
+
+    public WeaponID ItemID(int index)
+    {
+        return shopItems[index].ID;
+    }
+
+    public int EnergyCost()
+    {
+        return _energyCost[_currentEnergy];
+    }
+
+    public void UpdateView(int energy, int money)
+    {
+        _currentEnergy = energy;
+
+        _energyCount.text = _energyView[energy];
+        _energyButton.Init(_energyCost[energy]);
+        _energyButton.Interactable(_energyCost[energy] <= money);
+
+        for (int i = 0; i < shopItems.Length; i++)
+        {
+            shopItems[i].button.Interactable(shopItems[i].cost <= money);
+        }
+    }
+}
+
+[System.Serializable]
+public struct ShopItem
+{
+    public UI_ShopItem button;
+    public WeaponID ID;
+    public int cost;
+}
---
+++ EP28 - Shop/UI_ShopItem.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_ShopItem : MonoBehaviour
+{
+    [SerializeField]
+    private Text _title;
+    [SerializeField]
+    private Text _Textcost;
+    [SerializeField]
+    private Image _icon;
+
+    public void Init(int cost)
+    {
+        _Textcost.text = "$ " + cost;
+    }
+
+    public void Interactable(bool b)
+    {
+        GetComponent<Button>().interactable = b;
+
+        if (!b)
+        {
+            if (_icon)
+                _icon.color = Color.white;
+            _title.color = Color.white;
+            _Textcost.color = Color.red;
+        }
+        else
+        {
+            if (_icon)
+                _icon.color = Color.black;
+            _title.color = Color.black;
+            _Textcost.color = Color.green;
+        }
+    }
+}
